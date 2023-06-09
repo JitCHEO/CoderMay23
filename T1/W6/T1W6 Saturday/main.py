29c7a7a0e0ead7ea5092c9c0919b6d32f0131fd8
JitCHEO <adamcjk@gmail.com>
2023-06-10 09:34:55 +1000

    w6

---
+++ T1/W6/T1W6 Saturday/main.py
@@ -0,0 +1,14 @@
+
+# input and output
+s = input("Enter your marks in portfolio assignment (95)> ")
+print("Hello!", type(s))
+
+
+# pythin 3.6 and above string format method
+teacher = "akash"
+student = "dillon"
+
+sen = f"{teacher} is a teacher in coder academy, {student} is a student in may batch"
+# sen = "{} is a teacher in coder academy, {} is a student in may batch".format(teacher, student) older python version
+
+print(sen)
