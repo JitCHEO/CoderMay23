2712dac8ddf7e757446fadf5c4989683b17e6145
JIT KWONG CHEO <jitkcheo@JITs-MacBook-Pro.local>
2023-10-26 19:42:54 +1000

    try-catch part 2

--- T3(i)/W2/T2WThurs/messageList.js
+++ T3(i)/W2/T2WThurs/messageList.js
@@ -1,10 +1,21 @@
-let messages = [
-    "Callum",
-    "Jit",
-    "Connor",
-    "Lukas"
-]
+const fs = require('fs')
 
+let messages = []
+
+function getNames(file){
+    let data = null
+    try{
+        data = fs.readFileSync(file, 'utf-8');
+    } catch (error){
+        console.warn("Error reading from the file")
+        console.error(error.message)
+    }
+
+    console.log(data);
+    console.log(typeof(data));
+}
+
+getNames("names.txt");
 module.exports = {
     names: messages
 }
