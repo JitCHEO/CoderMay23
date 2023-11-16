38b0fe2e03e7924631f2365b19e149ebc57931d9
JIT KWONG CHEO <jitkcheo@192-168-1-100.tpgi.com.au>
2023-11-16 20:04:24 +1000

    react-bootstrap

--- T3(i)/W5/T3W5ThursReactJS /may23-t3w5-reactjs/src/App.js
+++ T3(i)/W5/T3W5ThursReactJS /may23-t3w5-reactjs/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+// import './App.css';
 import FruitList from './FruitList';
 import Title from './Title';
 import Footer from './Footer';
@@ -11,7 +11,7 @@ function App() {
       <FruitList colour = {"Yellow"} listToShow = {["mangoes", "bananas" , "lemon", "pineapple"]}/>
       <FruitList colour = {"Green"} listToShow = {["lime", "pears" , "apples"]}/>
       <FruitList colour = {"Red"} listToShow = {["strawberry", "cherry" , "blood orange"]}>
-        <h5>Red fruits are delicious</h5>
+        <h5 className="listMessage">Red fruits are delicious</h5>
         <h6>And great for you!</h6>
       </FruitList>
       <Footer dataFromApi="Random data"/>
--- T3(i)/W5/T3W5ThursReactJS /may23-t3w5-reactjs/src/FruitList.jsx
+++ T3(i)/W5/T3W5ThursReactJS /may23-t3w5-reactjs/src/FruitList.jsx
@@ -1,14 +1,16 @@
 // Array of fruit names:
 
-let someFruit = [
-  "mangoes",
-  "pears",
-  "oranges",
-  "bananas",
-  "strawberries",
-  "apples"
-];
+// let someFruit = [
+//   "mangoes",
+//   "pears",
+//   "oranges",
+//   "bananas",
+//   "strawberries",
+//   "apples"
+// ];
 
+import './FruitList.css';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 export default function FruitList (props) {
   // it's not a good practice to use index as a key, if it's an array of objects, use the object id as a key
@@ -17,8 +19,21 @@ export default function FruitList (props) {
     return null;
   }
 
-  let fruitListItems = props.listToShow.map((fruit => <li key={fruit}>{fruit}</li>))
-  console.log(props.children);
+  // Using React-bootstrap
+  // let fruitListItems = props.listToShow.map((fruit => <ListGroup.Item className="listMessage" key={fruit}>{fruit}</ListGroup.Item>))
+  // // console.log(props.children);
+  // return (
+  //   <>
+  //     <h3>{props.colour} fruits</h3>
+  //     <ListGroup>
+  //       {fruitListItems}
+  //     </ListGroup>
+  //     {props.children}
+  //   </>
+  
+  // Standard li & ul
+  let fruitListItems = props.listToShow.map((fruit => <li className="listMessage" key={fruit}>{fruit}</li>))
+  // console.log(props.children);
   return (
     <>
       <h3>{props.colour} fruits</h3>
