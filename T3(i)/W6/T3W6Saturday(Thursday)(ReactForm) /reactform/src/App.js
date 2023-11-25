fdbe4af3253555d1b3b19b54e132c835d4568ae4
JIT KWONG CHEO <jitkcheo@192-168-1-100.tpgi.com.au>
2023-11-25 10:18:04 +1000

    create 3 routes

--- T3(i)/W6/T3W6Saturday(Thursday)(ReactForm) /reactform/src/App.js
+++ T3(i)/W6/T3W6Saturday(Thursday)(ReactForm) /reactform/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import BusincessCardPage from './pages/BusinessCardPage';
+import { Routes, Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/cards' element={<BusincessCardPage />} />
+      </Routes>
     </div>
   );
 }
