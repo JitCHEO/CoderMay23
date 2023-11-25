16246165dbeeed4cb7ac4e69c4bba01047a4e654
JIT KWONG CHEO <jitkcheo@192-168-1-100.tpgi.com.au>
2023-11-25 10:04:48 +1000

    install react router & create the pages

---
+++ T3(i)/W6/T3W6Saturday(Thursday)(ReactForm) /reactform/src/pages/BusinessCardPage.jsx
@@ -0,0 +1,12 @@
+import BusinessCardParent from "../BusinessCardParent"
+
+export default function BusincessCardPage (){
+    return(
+        <div>
+            <BusinessCardParent />
+            <BusinessCardParent />
+            <BusinessCardParent />
+            <BusinessCardParent />
+        </div>
+    )
+}
