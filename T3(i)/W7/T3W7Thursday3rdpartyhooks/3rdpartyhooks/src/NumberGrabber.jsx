454764085b23ad0030f26197278230548dacf2f0
JIT KWONG CHEO <jitkcheo@192-168-1-100.tpgi.com.au>
2023-11-30 19:52:55 +1000

    useNavigate implemented

--- T3(i)/W7/T3W7Thursday3rdpartyhooks/3rdpartyhooks/src/NumberGrabber.jsx
+++ T3(i)/W7/T3W7Thursday3rdpartyhooks/3rdpartyhooks/src/NumberGrabber.jsx
@@ -1,14 +1,32 @@
-import { useParams } from "react-router-dom"
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom"
 
 export default function NumberGraber (){
 
     //const params = useParams();
     const {banana, id, potato} = useParams();
+    const navigate = useNavigate();
+
+    function navigateToHome(){
+        console.log("The logic that posts a message, submits a form or whatever")
+        navigate('/')
+    }
+
+    useEffect(()=> {
+        setTimeout(() => {
+            console.log("Simulating we're doing something, we will now redirect you...");
+            navigate('/');
+        }, 5000)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+    
     return(
         <div>
             <h1>The number that this component grabbed is: {banana}</h1>
             <h1>And the id is: {id}</h1>
             <h1>And potatoes: {potato}</h1>
+            {/* <button onClick={()=>{navigate("/")}}>Go home</button> */}
+            <button onClick={navigateToHome}>Go home</button>
         </div>    
     )
 }
