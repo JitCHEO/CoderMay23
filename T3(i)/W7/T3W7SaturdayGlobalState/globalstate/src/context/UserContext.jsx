b4fc2ff9304fa921a59206e7677c17a3d6916c56
JIT KWONG CHEO <jitkcheo@192-168-1-100.tpgi.com.au>
2023-12-02 11:14:36 +1000

    reducer passed into context

--- T3(i)/W7/T3W7SaturdayGlobalState/globalstate/src/context/UserContext.jsx
+++ T3(i)/W7/T3W7SaturdayGlobalState/globalstate/src/context/UserContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import { userReducer } from "./userReducer";
 
 const defaultUserData = {
     jwt: 'not empty jwt'
@@ -7,12 +8,13 @@ const defaultUserData = {
 export const UserContext = createContext(defaultUserData);
 
 export default function UserGlobalData(props){
-    const [userData, setUserData] = useState(defaultUserData)
+    // const [userData, setUserData] = useState(defaultUserData);
+    const [userData, userDataDispatch] = useReducer(userReducer, defaultUserData);
     return(
         <UserContext.Provider value={
             {
                 userData: userData, 
-                setUserData: setUserData
+                userDataDispatch: userDataDispatch
             }
         }>
             {props.children}
