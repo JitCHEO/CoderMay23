e478bf8485ea5a4d559e1bed2d80f7c3fe147c73
JitCHEO <adamcjk@gmail.com>
2023-12-19 18:47:06 +1000

    listen and first route

--- T3(i)/W10/T3W10TuesdayExpressIntro/index.js
+++ T3(i)/W10/T3W10TuesdayExpressIntro/index.js
@@ -1,1 +1,20 @@
-console.log("hey everyone, server WIP")
+// console.log("hey everyone, server WIP...");
+//import the server package
+const express = require('express');
+
+// make an instance of the server that we can customise and run
+const app = express();
+
+// write the routes
+//GET localhost:3000/
+// app.get(route path, callback function to handle request and response)
+app.get('/', (request, response) => {
+    response.send("Hello world, server building in progress...")
+})
+
+// Configure the server
+
+// Activate the server
+app.listen(3000,()=> {
+    console.log("Server is running on port 3000");
+})
