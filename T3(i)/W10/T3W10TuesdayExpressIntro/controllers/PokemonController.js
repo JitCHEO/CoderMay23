bc70a8f7eb9d556dfa4a7d12e33edee887f276da
JitCHEO <adamcjk@gmail.com>
2023-12-19 19:43:03 +1000

    async API call from the server

--- T3(i)/W10/T3W10TuesdayExpressIntro/controllers/PokemonController.js
+++ T3(i)/W10/T3W10TuesdayExpressIntro/controllers/PokemonController.js
@@ -9,6 +9,16 @@ router.get("/", (request, response) => {
     });
 })
 
+router.get("/pikachu", async (request, response) => {
+    let result = await fetch("https://pokeapi.co/api/v2/pokemon/25");
+    let data = await result.json();
+
+    response.json({
+        message: "A pokemon from the API, what's your name?",
+        pokemonName: data.name
+    })
+})
+
 router.post('/', (req, res) => {
     res.json({
         message: "POST request received!"
