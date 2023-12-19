248136102d777f22eddbcaa45a05bf9d266d5352
JitCHEO <adamcjk@gmail.com>
2023-12-19 19:33:53 +1000

    routes in the controller

--- T3(i)/W10/T3W10TuesdayExpressIntro/index.js
+++ T3(i)/W10/T3W10TuesdayExpressIntro/index.js
@@ -5,16 +5,23 @@ const express = require('express');
 // make an instance of the server that we can customise and run
 const app = express();
 
+const HOST = process.env.HOST || "localhost"
+const PORT = process.env.PORT || 3000;
+//ports run in integers between 1001 - 65536
+
 // write the routes
 //GET localhost:3000/
 // app.get(route path, callback function to handle request and response)
 app.get('/', (request, response) => {
-    response.send("Hello world, server building in progress...")
+    response.send("Hello world, server building in progress...");
 })
+
+const pokemonRouter = require('./controllers/PokemonController');
+app.use("/pokemon", pokemonRouter);
 
 // Configure the server
 
 // Activate the server
-app.listen(3000,()=> {
-    console.log("Server is running on port 3000");
+app.listen(PORT, HOST,()=> {
+    console.log(`Server is running on port ${PORT}`);
 })
