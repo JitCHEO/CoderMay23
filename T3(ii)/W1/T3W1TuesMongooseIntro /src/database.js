649d842ccb6ede46c81276456651dad522aac417
JitCHEO <adamcjk@gmail.com>
2024-01-16 19:58:26 +1000

    db table created using terminal ^ mongodb atlas

--- T3(ii)/W1/T3W1TuesMongooseIntro /src/database.js
+++ T3(ii)/W1/T3W1TuesMongooseIntro /src/database.js
@@ -7,7 +7,13 @@ const mongoose = require('mongoose');
 async function databaseConnect(){
     try{
         //DB connection can take some time, especially if the DB is in the cloud
-        await mongoose.connect('mongodb://localhost:27017/PetDB');
+        //await mongoose.connect('mongodb://localhost:27017/PetDB');
+            // Goes into .env file to avoid sharing the login & passwords
+             // await mongoose.connect('mongodb+srv://<username>:<password>@may23.q5tuvnr.mongodb.net/?retryWrites=true&w=majority')
+             // await mongoose.connect('mongodb+srv://pet_db_admin:pet_db_admin_1@may23.q5tuvnr.mongodb.net/?retryWrites=true&w=majority')
+
+        await mongoose.connect(process.env.DB_URI);
+
         console.log("Database connected");
     } catch (error) {
         console.warn(`database connection failed: ${JSON.stringify(error)}`)
