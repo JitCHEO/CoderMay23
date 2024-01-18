0997cce4b85fc4dd1f540871c8f11f206d760261
JitCHEO <adamcjk@gmail.com>
2024-01-18 18:47:34 +1000

    User schema

---
+++ T3(ii)/W1/T3W1ThursMongooseIntrocontinue/src/models/UserModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+// Option 1:
+// // const Schema = mongoose.Schema;
+
+// const UserSchema = new mongoose.Schema({
+
+// })
+
+// Option 2:
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: {
+        typw: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: false // attributes are not unique by default
+    } 
+})
+
+// const ModelName = mongoose.model('Name that appears in MongoDB(local or Atlas)',SchemaThatModelIsBasedOn)
+const User = mongoose.model('User', UserSchema);
+
+module.exports = {
+    User
+}
